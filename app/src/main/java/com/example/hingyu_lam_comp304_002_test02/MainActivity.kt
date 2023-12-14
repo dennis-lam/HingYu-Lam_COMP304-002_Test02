34b30aef6a087406e48aca6fbd214be34e0a185b
Hing Yu Lam <hlam90@my.centennialcollege.ca>
2023-12-14 10:43:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hingyu_lam_comp304_002_test02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hingyu_lam_comp304_002_test02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
