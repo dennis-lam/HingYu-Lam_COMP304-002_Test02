9ae2e7b305ab31716111399c80d784f1665b7700
Hing Yu Lam <hlam90@my.centennialcollege.ca>
2023-12-14 10:48:49 -0500

    Gradle

--- app/src/main/java/com/example/hingyu_lam_comp304_002_test02/VaccineDetailsActivity.kt
+++ app/src/main/java/com/example/hingyu_lam_comp304_002_test02/VaccineDetailsActivity.kt
@@ -3,9 +3,9 @@ package com.example.hingyu_lam_comp304_002_test02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class VaccineDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_vaccine_details)
     }
 }
---
+++ app/src/main/java/com/example/hingyu_lam_comp304_002_test02/VaccineLocationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hingyu_lam_comp304_002_test02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VaccineLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vaccine_location)
+    }
+}
